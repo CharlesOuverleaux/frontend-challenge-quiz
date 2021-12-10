17a849c5217f13ee54d460931b887427aba44005
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-10 15:37:48 +0100

    added local storage (backend)

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-// Hook
+// Hook fc
 function useLocalStorage(key, initialValue) {
   // State to store our value
   // Pass initial state function to useState so logic is only executed once
