833463153171d5bdea838bd82ae0e3d7d9c2e776
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-11 11:53:30 +0100

    Added useToggle hook

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import { useState } from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
 import ScoreMessage from './components/ScoreMessage';
+import useToggle from './hooks/useToggleState';
 
 function App() {
-
-  const [isSubmitted, setisSubmitted] = useState(false);
-
-  const handleSubmit = (isSubmitted) => {
-      setisSubmitted(!isSubmitted);
-
-  }
+  const [isSubmitted, toggle] = useToggle(false);
 
   return (
     <div className="App">
       { isSubmitted ? <ScoreMessage /> : <Quiz />}
-      <button onClick={() => handleSubmit(isSubmitted)}>{isSubmitted ? 'Go back': 'Submit my answers'}</button>
+      <button onClick={toggle}>{isSubmitted ? 'Go back': 'Submit my answers'}</button>
     </div>
   );
 }
