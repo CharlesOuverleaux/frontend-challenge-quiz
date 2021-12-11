b8b04fbcedb5437b669fbdfabd0753170f85bc49
Charles Ouverleaux <33698505+CharlesOuverleaux@users.noreply.github.com>
2021-12-11 11:32:26 +0100

    Added score component logic, pulling data from LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
 import { useState } from 'react';
 import './App.css';
 import Quiz from './components/Quiz';
-import useLocalStorage from './hooks/useLocalStorage';
-import { data } from './data/data'
 import ScoreMessage from './components/ScoreMessage';
 
 function App() {
 
   const [isSubmitted, setisSubmitted] = useState(false);
-  const [score, setScore] = useState(0);
-  const [questions, setQuestions] = useLocalStorage("questions", data);
 
   const handleSubmit = (isSubmitted) => {
       setisSubmitted(!isSubmitted);
-      let count = 0
-      questions.forEach((question) =>
-        question.answerCorrect ? count++ : count
-      )
-      setScore(count)
+
   }
 
   return (
     <div className="App">
-      { isSubmitted ? <ScoreMessage score={score}/> : <Quiz />}
+      { isSubmitted ? <ScoreMessage /> : <Quiz />}
       <button onClick={() => handleSubmit(isSubmitted)}>{isSubmitted ? 'Go back': 'Submit my answers'}</button>
     </div>
   );
--- src/components/ScoreMessage.js
+++ src/components/ScoreMessage.js
@@ -1,7 +1,15 @@
-export default function ScoreMessage(score){
+
+export default function ScoreMessage(){
+  const results = JSON.parse(localStorage.getItem('questions'))
+
+  let count = 0;
+  results.forEach((result) =>
+    result.answerCorrect ? count++ : count
+  );
+
   return (
     <div className="ScoreMessage">
-      <p>Your score is: {score.score} out of 5</p>
+      <p>Your score is: {count} out of 5</p>
     </div>
   );
 }
